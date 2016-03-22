8919d5fc584914a2950552fff269a8bde25d1568
Antony <anthonykalogeropoulos@gmail.com>
2016-03-22 16:46:41 +0200

    Create front-end for Admin Panel

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -8,7 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Gentallela Alela! | </title>
+	<title>Admin Panel | BusTracker </title>
 
 	<!-- Bootstrap core CSS -->
 
@@ -19,12 +19,10 @@
 
 	<!-- Custom styling plus plugins -->
 	<link href="css/custom.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="css/maps/jquery-jvectormap-2.0.3.css" />
-	<link href="css/icheck/flat/green.css" rel="stylesheet" />
-	<link href="css/floatexamples.css" rel="stylesheet" type="text/css" />
+	<link href="css/icheck/flat/green.css" rel="stylesheet">
+
 
 	<script src="js/jquery.min.js"></script>
-	<script src="js/nprogress.js"></script>
 
 	<!--[if lt IE 9]>
 	<script src="../assets/js/ie8-responsive-file-warning.js"></script>
@@ -42,23 +40,26 @@
 <body class="nav-md">
 
 <div class="container body">
+
+
 	<div class="main_container">
+
 		<div class="col-md-3 left_col">
 			<div class="left_col scroll-view">
 
 				<div class="navbar nav_title" style="border: 0;">
-					<a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Empty!</span></a>
+					<a href="" class="site_title"><i class="fa fa-bus"></i> <span>Admin Panel</span></a>
 				</div>
 				<div class="clearfix"></div>
 
 				<!-- menu prile quick info -->
 				<div class="profile">
 					<div class="profile_pic">
-						<img src="images/img.jpg" alt="..." class="img-circle profile_img">
+						<img src="img/dereeOfficialLogo.jpg" alt="DEREE Logo" class="img-circle profile_img">
 					</div>
 					<div class="profile_info">
 						<span>Welcome,</span>
-						<h2>Empty user</h2>
+						<h2>Administrator</h2>
 					</div>
 				</div>
 				<!-- /menu prile quick info -->
@@ -69,26 +70,51 @@
 				<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
 					<div class="menu_section">
-						<h3>General</h3>
+						<h3> Manage </h3>
+						<hr/>
 						<ul class="nav side-menu">
-							<li><a><i class="fa fa-home"></i> Menu1 <span class="fa fa-chevron-down"></span></a>
+							<li><a><i class="fa fa-road"></i> Routes <span class="fa fa-chevron-down"></span></a>
 								<ul class="nav child_menu" style="display: none">
-									<li><a href="empty.html">Sub1.1</a>
+									<li><a href="#">Create Route</a>
 									</li>
-									<li><a href="empty.html">Sub1.2</a>
+									<li><a href="#">Delete Route</a>
+									</li>
+									<li><a href="#">Update Route</a>
 									</li>
 								</ul>
 							</li>
-							<li><a><i class="fa fa-edit"></i> Menu2 <span class="fa fa-chevron-down"></span></a>
+							<li><a><i class="fa fa-map-marker"></i> Bus-Stops <span class="fa fa-chevron-down"></span></a>
 								<ul class="nav child_menu" style="display: none">
-									<li><a href="empty.html">Menu2.1</a>
+									<li><a href="#">Create Stop</a></li>
+									<li><a href="#">Delete Stop</a></li>
+									<li><a href="#">Update Stop</a></li>
+								</ul>
+							</li>
+							<li><a><i class="fa fa-line-chart"></i> Statistics <span class="fa fa-chevron-down"></span></a>
+								<ul class="nav child_menu" style="display: none">
+									<li><a href="#">General</a>
 									</li>
-									<li><a href="empty.html">Meny2.2s</a>
+									<li><a href="#">Other</a>
 									</li>
 								</ul>
 							</li>
 						</ul>
 					</div>
+					<div class="menu_section">
+						<h3>Need Help?</h3>
+						<hr/>
+						<ul class="nav side-menu">
+							<li><a><i class="fa fa-question-circle"></i> Contact Support <span class="fa fa-chevron-down"></span></a>
+								<ul class="nav child_menu" style="display: none">
+									<li><a href="#">E-mail</a>
+									</li>
+									<li><a href="#">Call</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+
 				</div>
 				<!-- /sidebar menu -->
 
@@ -119,53 +145,165 @@
 					<div class="nav toggle">
 						<a id="menu_toggle"><i class="fa fa-bars"></i></a>
 					</div>
+
+					<ul class="nav navbar-nav navbar-right">
+						<li class="">
+							<a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+<!--								<img src="img/dereeLogo.png" alt="Admin Image">-->
+								<b>Admin</b>
+								<span class="fa fa-chevron-down"></span>
+							</a>
+							<ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
+								<li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+								</li>
+							</ul>
+						</li>
+
+<!--Notification Bar-->
+<!--
+						<li role="presentation" class="dropdown">
+							<a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+								<i class="fa fa-envelope-o"></i>
+								<span class="badge bg-green">6</span>
+							</a>
+							<ul id="menu1" class="dropdown-menu list-unstyled msg_list animated fadeInDown" role="menu">
+								<li>
+									<a>
+                      <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+									</a>
+								</li>
+								<li>
+									<a>
+                      <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+									</a>
+								</li>
+								<li>
+									<a>
+                      <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+									</a>
+								</li>
+								<li>
+									<a>
+                      <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>John Smith</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+									</a>
+								</li>
+								<li>
+									<div class="text-center">
+										<a>
+											<strong>See All Alerts</strong>
+											<i class="fa fa-angle-right"></i>
+										</a>
+									</div>
+								</li>
+							</ul>
+						</li>
+-->
+
+					</ul>
 				</nav>
 			</div>
 
 		</div>
 		<!-- /top navigation -->
 
-
 		<!-- page content -->
 		<div class="right_col" role="main">
+			<div class="">
+				<div class="page-title">
+					<div class="title_left">
+						<h3>Plain Page</h3>
+					</div>
 
-			<div class="row">
-				<div class="col-md-12 col-sm-12 col-xs-12">
-					<div class="dashboard_graph">
-
-						<div class="row x_title">
-							<div class="col-md-6">
-								<h3>Empty <small>no content</small></h3>
-							</div>
-							<div class="col-md-6">
-
+					<div class="title_right">
+						<div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="Search for...">
+                  <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
 							</div>
 						</div>
-
-						<div class="col-md-12 col-sm-12 col-xs-12">
-							...
-						</div>
-
-						<div class="clearfix"></div>
 					</div>
 				</div>
+				<div class="clearfix"></div>
 
+				<div class="row">
+
+					<div class="col-md-12 col-sm-12 col-xs-12">
+						<div class="x_panel" style="height:600px;">
+							<div class="x_title">
+								<h2>Plain Page</h2>
+								<ul class="nav navbar-right panel_toolbox">
+									<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+										<ul class="dropdown-menu" role="menu">
+											<li><a href="#">Settings 1</a>
+											</li>
+											<li><a href="#">Settings 2</a>
+											</li>
+										</ul>
+									</li>
+									<li><a class="close-link"><i class="fa fa-close"></i></a>
+									</li>
+								</ul>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<br />
 
 			<!-- footer content -->
-
 			<footer>
 				<div class="copyright-info">
-					<p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+					<p class="pull-right">
+						<a href="http://ashoka.students.acg.edu/BusTracker/public/"> Go to Application </a>
 					</p>
 				</div>
 				<div class="clearfix"></div>
 			</footer>
 			<!-- /footer content -->
+
 		</div>
 		<!-- /page content -->
-
 	</div>
 
 </div>
@@ -179,254 +317,17 @@
 
 <script src="js/bootstrap.min.js"></script>
 
-<!-- gauge js -->
-<script type="text/javascript" src="js/gauge/gauge.min.js"></script>
-<script type="text/javascript" src="js/gauge/gauge_demo.js"></script>
-<!-- chart js -->
-<script src="js/chartjs/chart.min.js"></script>
 <!-- bootstrap progress js -->
 <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
 <script src="js/nicescroll/jquery.nicescroll.min.js"></script>
 <!-- icheck -->
 <script src="js/icheck/icheck.min.js"></script>
-<!-- daterangepicker -->
-<script type="text/javascript" src="js/moment/moment.min.js"></script>
-<script type="text/javascript" src="js/datepicker/daterangepicker.js"></script>
 
 <script src="js/custom.js"></script>
 
-<!-- flot js -->
-<!--[if lte IE 8]><script type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
-<script type="text/javascript" src="js/flot/jquery.flot.js"></script>
-<script type="text/javascript" src="js/flot/jquery.flot.pie.js"></script>
-<script type="text/javascript" src="js/flot/jquery.flot.orderBars.js"></script>
-<script type="text/javascript" src="js/flot/jquery.flot.time.min.js"></script>
-<script type="text/javascript" src="js/flot/date.js"></script>
-<script type="text/javascript" src="js/flot/jquery.flot.spline.js"></script>
-<script type="text/javascript" src="js/flot/jquery.flot.stack.js"></script>
-<script type="text/javascript" src="js/flot/curvedLines.js"></script>
-<script type="text/javascript" src="js/flot/jquery.flot.resize.js"></script>
-<script>
-	$(document).ready(function() {
-		// [17, 74, 6, 39, 20, 85, 7]
-		//[82, 23, 66, 9, 99, 6, 2]
-		var data1 = [
-			[gd(2012, 1, 1), 17],
-			[gd(2012, 1, 2), 74],
-			[gd(2012, 1, 3), 6],
-			[gd(2012, 1, 4), 39],
-			[gd(2012, 1, 5), 20],
-			[gd(2012, 1, 6), 85],
-			[gd(2012, 1, 7), 7]
-		];
-
-		var data2 = [
-			[gd(2012, 1, 1), 82],
-			[gd(2012, 1, 2), 23],
-			[gd(2012, 1, 3), 66],
-			[gd(2012, 1, 4), 9],
-			[gd(2012, 1, 5), 119],
-			[gd(2012, 1, 6), 6],
-			[gd(2012, 1, 7), 9]
-		];
-		$("#canvas_dahs").length && $.plot($("#canvas_dahs"), [
-			data1, data2
-		], {
-			series: {
-				lines: {
-					show: false,
-					fill: true
-				},
-				splines: {
-					show: true,
-					tension: 0.4,
-					lineWidth: 1,
-					fill: 0.4
-				},
-				points: {
-					radius: 0,
-					show: true
-				},
-				shadowSize: 2
-			},
-			grid: {
-				verticalLines: true,
-				hoverable: true,
-				clickable: true,
-				tickColor: "#d5d5d5",
-				borderWidth: 1,
-				color: '#fff'
-			},
-			colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
-			xaxis: {
-				tickColor: "rgba(51, 51, 51, 0.06)",
-				mode: "time",
-				tickSize: [1, "day"],
-				//tickLength: 10,
-				axisLabel: "Date",
-				axisLabelUseCanvas: true,
-				axisLabelFontSizePixels: 12,
-				axisLabelFontFamily: 'Verdana, Arial',
-				axisLabelPadding: 10
-				//mode: "time", timeformat: "%m/%d/%y", minTickSize: [1, "day"]
-			},
-			yaxis: {
-				ticks: 8,
-				tickColor: "rgba(51, 51, 51, 0.06)",
-			},
-			tooltip: false
-		});
-
-		function gd(year, month, day) {
-			return new Date(year, month - 1, day).getTime();
-		}
-	});
-</script>
-
-<!-- worldmap -->
-<script type="text/javascript" src="js/maps/jquery-jvectormap-2.0.3.min.js"></script>
-<script type="text/javascript" src="js/maps/gdp-data.js"></script>
-<script type="text/javascript" src="js/maps/jquery-jvectormap-world-mill-en.js"></script>
-<script type="text/javascript" src="js/maps/jquery-jvectormap-us-aea-en.js"></script>
 <!-- pace -->
 <script src="js/pace/pace.min.js"></script>
-<script>
-	$(function() {
-		$('#world-map-gdp').vectorMap({
-			map: 'world_mill_en',
-			backgroundColor: 'transparent',
-			zoomOnScroll: false,
-			series: {
-				regions: [{
-					values: gdpData,
-					scale: ['#E6F2F0', '#149B7E'],
-					normalizeFunction: 'polynomial'
-				}]
-			},
-			onRegionTipShow: function(e, el, code) {
-				el.html(el.html() + ' (GDP - ' + gdpData[code] + ')');
-			}
-		});
-	});
-</script>
-<!-- skycons -->
-<script src="js/skycons/skycons.min.js"></script>
-<script>
-	var icons = new Skycons({
-			"color": "#73879C"
-		}),
-		list = [
-			"clear-day", "clear-night", "partly-cloudy-day",
-			"partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
-			"fog"
-		],
-		i;
 
-	for (i = list.length; i--;)
-		icons.set(list[i], list[i]);
-
-	icons.play();
-</script>
-
-<!-- dashbord linegraph -->
-<script>
-	var doughnutData = [{
-		value: 30,
-		color: "#455C73"
-	}, {
-		value: 30,
-		color: "#9B59B6"
-	}, {
-		value: 60,
-		color: "#BDC3C7"
-	}, {
-		value: 100,
-		color: "#26B99A"
-	}, {
-		value: 120,
-		color: "#3498DB"
-	}];
-	var myDoughnut = new Chart(document.getElementById("canvas1").getContext("2d")).Doughnut(doughnutData);
-</script>
-<!-- /dashbord linegraph -->
-<!-- datepicker -->
-<script type="text/javascript">
-	$(document).ready(function() {
-
-		var cb = function(start, end, label) {
-			console.log(start.toISOString(), end.toISOString(), label);
-			$('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-			//alert("Callback has fired: [" + start.format('MMMM D, YYYY') + " to " + end.format('MMMM D, YYYY') + ", label = " + label + "]");
-		}
-
-		var optionSet1 = {
-			startDate: moment().subtract(29, 'days'),
-			endDate: moment(),
-			minDate: '01/01/2012',
-			maxDate: '12/31/2015',
-			dateLimit: {
-				days: 60
-			},
-			showDropdowns: true,
-			showWeekNumbers: true,
-			timePicker: false,
-			timePickerIncrement: 1,
-			timePicker12Hour: true,
-			ranges: {
-				'Today': [moment(), moment()],
-				'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-				'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-				'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-				'This Month': [moment().startOf('month'), moment().endOf('month')],
-				'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-			},
-			opens: 'left',
-			buttonClasses: ['btn btn-default'],
-			applyClass: 'btn-small btn-primary',
-			cancelClass: 'btn-small',
-			format: 'MM/DD/YYYY',
-			separator: ' to ',
-			locale: {
-				applyLabel: 'Submit',
-				cancelLabel: 'Clear',
-				fromLabel: 'From',
-				toLabel: 'To',
-				customRangeLabel: 'Custom',
-				daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-				monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-				firstDay: 1
-			}
-		};
-		$('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
-		$('#reportrange').daterangepicker(optionSet1, cb);
-		$('#reportrange').on('show.daterangepicker', function() {
-			console.log("show event fired");
-		});
-		$('#reportrange').on('hide.daterangepicker', function() {
-			console.log("hide event fired");
-		});
-		$('#reportrange').on('apply.daterangepicker', function(ev, picker) {
-			console.log("apply event fired, start/end dates are " + picker.startDate.format('MMMM D, YYYY') + " to " + picker.endDate.format('MMMM D, YYYY'));
-		});
-		$('#reportrange').on('cancel.daterangepicker', function(ev, picker) {
-			console.log("cancel event fired");
-		});
-		$('#options1').click(function() {
-			$('#reportrange').data('daterangepicker').setOptions(optionSet1, cb);
-		});
-		$('#options2').click(function() {
-			$('#reportrange').data('daterangepicker').setOptions(optionSet2, cb);
-		});
-		$('#destroy').click(function() {
-			$('#reportrange').data('daterangepicker').remove();
-		});
-	});
-</script>
-<script>
-	NProgress.done();
-</script>
-<!-- /datepicker -->
-<!-- /footer content -->
 </body>
 
 </html>
