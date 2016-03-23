54c891312f6a01cc6c5724427ec8f95baa229ec1
Antony <anthonykalogeropoulos@gmail.com>
2016-03-23 19:44:17 +0200

    Add temp text to admin_start View and Make div collapsing functional

--- app/Views/admin_start.php
+++ app/Views/admin_start.php
@@ -3,7 +3,7 @@
 			<div class="">
 				<div class="page-title">
 					<div class="title_left">
-						<h3>Plain Page</h3>
+						<h3>Main Section</h3>
 					</div>
 
 					<div class="title_right">
@@ -20,27 +20,18 @@
 				<div class="clearfix"></div>
 
 				<div class="row">
-
 					<div class="col-md-12 col-sm-12 col-xs-12">
 						<div class="x_panel" style="height:600px;">
 							<div class="x_title">
-								<h2>Plain Page</h2>
+								<h2>Manual</h2>
 								<ul class="nav navbar-right panel_toolbox">
 									<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
 									</li>
-									<li class="dropdown">
-										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-										<ul class="dropdown-menu" role="menu">
-											<li><a href="#">Settings 1</a>
-											</li>
-											<li><a href="#">Settings 2</a>
-											</li>
-										</ul>
-									</li>
-									<li><a class="close-link"><i class="fa fa-close"></i></a>
-									</li>
 								</ul>
 								<div class="clearfix"></div>
+							</div>
+							<div class="x_content">
+								<p> We will 'soon' put a nice explanation of how to use the Admin Panel here.</p>
 							</div>
 						</div>
 					</div>
